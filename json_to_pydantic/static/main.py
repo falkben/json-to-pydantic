a840be967755d3f322b63e72a84ca3f17fa79c04
Ben Falk <falk.ben@gmail.com>
2025-10-13 08:34:58 -0400

    micropip itself has to be installed

--- json_to_pydantic/static/main.py
+++ json_to_pydantic/static/main.py
@@ -1,8 +1,6 @@
 import asyncio
 import json
 
-import micropip
-
 
 def convert_to_schema(input_text: str, all_optional: bool, snake_case_field: bool):
     from datamodel_code_generator.parser.jsonschema import JsonSchemaParser
@@ -48,6 +46,10 @@ def convert():
 
 async def load_deps():
     # install without deps using micropip to avoid several deps
+    await micropip.install("micropip")  # noqa: F821
+
+    import micropip
+
     await micropip.install(
         "https://files.pythonhosted.org/packages/a3/44/bd5baa652b4d57853fc71c45bc0d25e1e28c92c54fcc6da07510fdd31ed7/datamodel_code_generator-0.21.1-py3-none-any.whl",
         deps=False,
